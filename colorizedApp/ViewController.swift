611821d5c5f5ef1a82e0958908bf1a84a38d88cc
Darina Samokhina <mokhinsam@yandex.ru>
2022-09-28 13:36:22 +0300

    Initial Commit

---
+++ colorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  colorizedApp
+//
+//  Created by Дарина Самохина on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

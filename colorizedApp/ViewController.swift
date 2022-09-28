269b333f6fa18a7bcc4993771a8c8f043f14abcf
Darina Samokhina <mokhinsam@yandex.ru>
2022-09-28 17:54:06 +0300

    set storyboard, set all logic

--- colorizedApp/ViewController.swift
+++ colorizedApp/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IB Outlets
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redCounter: UILabel!
+    @IBOutlet var greenCounter: UILabel!
+    @IBOutlet var blueCounter: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 15
+    
+        setCounter()
+        setColor()
     }
-
-
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider:
+            redCounter.text = string(from: redSlider)
+        case greenSlider:
+            greenCounter.text = string(from: greenSlider)
+        default:
+            blueCounter.text = string(from: blueSlider)
+        }
+    }
+  
+    //MARK: - Private Methods
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    private func setCounter() {
+        redCounter.text = string(from: redSlider)
+        greenCounter.text = string(from: greenSlider)
+        blueCounter.text = string(from: blueSlider)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+    
 }
 
